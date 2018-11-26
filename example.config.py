d47fd6528d21524e48caea1504a16846be8ffc96
Dan Kerchner <kerchner@gwu.edu>
2018-11-26 16:16:09 -0500

    Added sample config for using fake_rake.py

--- example.config.py
+++ example.config.py
@@ -4,3 +4,13 @@ ingest_command = "rvmsudo RAILS_ENV=production rake gwss:ingest_etd"
 ingest_depositor = "openaccess@gwu.edu"
 
 debug_mode = False
+
+# Example for fake_rake:
+"""
+ingest_path = "example/"
+# Command location is relative to ingest path
+ingest_command = "python ../fake_rake.py"
+ingest_depositor = "openaccess@gwu.edu"
+
+debug_mode = True
+"""
