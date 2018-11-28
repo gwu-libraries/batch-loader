6cc6683f15c14bc286998791532262e2081e8508
Dan Kerchner <kerchner@gwu.edu>
2018-11-28 15:18:06 -0500

    Fixes #15 but will require mod to GWSS code

--- batch_loader.py
+++ batch_loader.py
@@ -77,6 +77,11 @@ def create_repository_metadata(row, singular_field_names, repeating_field_names)
 
 
 def find_files(row_filepath, row_first_filepath, base_filepath):
+    if row_filepath is '':
+        # We will interpret this as "No files to load or update"
+        print("row_filepath is None")
+        return None, None
+
     filepath = os.path.join(base_filepath, row_filepath)
     if not os.path.exists(filepath):
         raise FileNotFoundError(filepath)
@@ -111,13 +116,16 @@ def repo_import(repo_metadata_filepath, title, first_file, other_files, reposito
     # rake gwss:ingest_etd -- --manifest='path-to-manifest-json-file' --primaryfile='path-to-primary-attachment-file/myfile.pdf' --otherfiles='path-to-all-other-attachments-folder'
     command = ingest_command.split(' ') + ['--',
                                            '--manifest=%s' % repo_metadata_filepath,
-                                           '--primaryfile=%s' % first_file,
                                            '--depositor=%s' % ingest_depositor]
+    if first_file:
+        command.extend(['--primaryfile=%s' % first_file])
     if other_files:
         command.extend(['--otherfiles=%s' % ','.join(other_files)])
     if repository_id:
         log.info('%s is an update.', title)
         command.extend(['--update-item-id=%s' % repository_id])
+        if first_file is None:
+            command.extend(['--skip-file-updates'])
     log.info("Command is: %s" % ' '.join(command))
     output = subprocess.check_output(command, cwd=ingest_path)
     repository_id = output.decode('utf-8').rstrip('\n')
